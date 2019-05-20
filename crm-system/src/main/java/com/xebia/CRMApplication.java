7c3a94674c9e6a5e0b3a61ccff600c2c75d56e45
upeter <peter.urs@gmail.com>
2019-05-20 10:09:33 +0200

    Main class and loglevels

--- crm-system/src/main/java/com/xebia/CRMApplication.java
+++ crm-system/src/main/java/com/xebia/CRMApplication.java
@@ -9,7 +9,6 @@ import com.amazonaws.services.sns.AmazonSNSClientBuilder;
 import com.amazonaws.services.sqs.AmazonSQS;
 import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.sun.tools.javac.util.List;
 import com.xebia.common.domain.Address;
 import com.xebia.common.domain.Customer;
 import com.xebia.common.service.CustomerRepository;
@@ -27,6 +26,8 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @SpringBootApplication
@@ -76,7 +77,7 @@ class DataLoader implements ApplicationRunner {
     }
 
     public void run(ApplicationArguments args) throws Exception{
-        List<Customer> customers = List.of(
+        List<Customer> customers = Arrays.asList(
                 new Customer("John Doe", "abc@efg.nl", "0612345678", true, true, new Address("Sesamstraat", "5b", "3456AB", "Amsterdam", "Netherlands")),
                 new Customer("Dave Burk", "dave@burk.nl", "0612345679", true, false, new Address("Beathovenlaan", "4", "4532AB", "Rotterdam", "Netherlands")),
                 new Customer("Jane Swam", "jane@hij.nl", "0612345680", false, true, new Address("Sunset Avenue", "4", "4455EF", "Utrecht", "Netherlands")));
