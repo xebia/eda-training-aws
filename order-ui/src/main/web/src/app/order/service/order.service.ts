e749d91983107c1c21808a536e807311fa1db621
Léon Rodenburg <l.rodenburg@instanceof.nl>
2019-07-18 15:34:50 +0200

    Allow switching of version

--- order-ui/src/main/web/src/app/order/service/order.service.ts
+++ order-ui/src/main/web/src/app/order/service/order.service.ts
@@ -1,19 +1,21 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Order} from '../model/order.model';
+import {VersionAwareService} from "../../shared/version-aware-service";
 
 @Injectable()
-export class OrderService {
+export class OrderService extends VersionAwareService {
     constructor(private http: HttpClient) {
+        super();
     }
 
-    baseUrl: string = 'http://localhost:9000/order-api/v1/orders';
+    baseUrl: string = 'http://localhost:9000/order-api';
 
     getOrders() {
-        return this.http.get<Order[]>(this.baseUrl);
+        return this.http.get<Order[]>(this.versionedUrl(this.baseUrl, 'orders'));
     }
 
     placeOrder(order: Order) {
-        return this.http.post(this.baseUrl, order);
+        return this.http.post(this.versionedUrl(this.baseUrl, 'orders'), order);
     }
 }
