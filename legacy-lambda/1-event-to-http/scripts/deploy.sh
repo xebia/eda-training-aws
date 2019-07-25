0a80748ac7696abc0aa7c18fc2a670707e468e47
Léon Rodenburg <l.rodenburg@instanceof.nl>
2019-07-25 11:44:35 +0200

    Add script for binding Lambda to SQS

--- legacy-lambda/1-event-to-http/scripts/deploy.sh
+++ legacy-lambda/1-event-to-http/scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Create zipfile of src directory
 TAG=$(date +%s)
-REGION=us-east-1
+REGION=eu-west-1
 STAGE=production
 LAMBDA_NAME=event-to-http-$TAG
 GATEWAY_NAME=$LAMBDA_NAME-$TAG-gateway
@@ -14,3 +14,11 @@ popd
 echo "Deploying function..."
 aws lambda create-function --function-name $LAMBDA_NAME --runtime python3.6 --handler handler.handler --zip-file fileb://out/$LAMBDA_NAME.zip --role arn:aws:iam::123456:role/irrelevant --region $REGION --endpoint http://localhost:4574
 LAMBDA_ARN=$(aws lambda list-functions --query "Functions[?FunctionName==\`$LAMBDA_NAME\`].FunctionArn" --output text --region $REGION --endpoint http://localhost:4574)
+
+# Bind function to `orderPlaced` SQS queue (assuming it was created)
+QUEUE_ARN=$(aws sqs get-queue-attributes --queue-url http://localhost:4576/queue/orderPlaced --attribute-name QueueArn --endpoint-url http://localhost:4576)
+aws lambda create-event-source-mapping \
+    --event-source-arn arn:aws:sqs:$REGION:queue:orderPlaced \
+    --function-name $LAMBDA_NAME \
+    --endpoint http://localhost:4574
+
--- legacy-lambda/1-event-to-http/src/handler.py
+++ legacy-lambda/1-event-to-http/src/handler.py
@@ -1,5 +1,17 @@
-import json
+# import json
 
 
 def handler(event, context):
-    return {"statusCode": 200, "body": json.dumps({"success": True})}
+    """
+    Exercise 4
+    Task: use the consumed SQS message in `event` to perform a POST call on the legacy Inventory service
+    For this exercise to succeed do the following:
+    - Grab the messages from the `event` parameter using event["Records"]. Multiple messages might be available.
+    - For each message:
+        - Grab the message body (using message["body"]) and parse the JSON
+        - Use the `requests` library to perform a HTTP POST to http://localhost:9010/inventory-api/v1/shipments
+        - Rely on the retry mechanism of SQS to make sure the POST is repeated until you get a 200 OK response code
+    """
+    records = event["Records"]
+    print(records)
+
