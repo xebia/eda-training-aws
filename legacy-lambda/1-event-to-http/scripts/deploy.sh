a89fde53dd9d111f16bee7aea5329a1485373814
Léon Rodenburg <l.rodenburg@instanceof.nl>
2019-07-25 13:02:04 +0200

    Finish event to HTTP lambda exercise

--- legacy-lambda/1-event-to-http/scripts/deploy.sh
+++ legacy-lambda/1-event-to-http/scripts/deploy.sh
@@ -6,8 +6,13 @@ STAGE=production
 LAMBDA_NAME=event-to-http-$TAG
 GATEWAY_NAME=$LAMBDA_NAME-$TAG-gateway
 
+# Build zipfile with vendored dependencies
 pushd src
-zip -r ../out/$LAMBDA_NAME.zip .
+zip -r9 ../out/$LAMBDA_NAME.zip .
+popd
+
+pushd vendor
+zip -r9 -g ../out/$LAMBDA_NAME.zip .
 popd
 
 # Deploy function to LocalStack
@@ -15,10 +20,15 @@ echo "Deploying function..."
 aws lambda create-function --function-name $LAMBDA_NAME --runtime python3.6 --handler handler.handler --zip-file fileb://out/$LAMBDA_NAME.zip --role arn:aws:iam::123456:role/irrelevant --region $REGION --endpoint http://localhost:4574
 LAMBDA_ARN=$(aws lambda list-functions --query "Functions[?FunctionName==\`$LAMBDA_NAME\`].FunctionArn" --output text --region $REGION --endpoint http://localhost:4574)
 
-# Bind function to `orderPlaced` SQS queue (assuming it was created)
-QUEUE_ARN=$(aws sqs get-queue-attributes --queue-url http://localhost:4576/queue/orderPlaced --attribute-name QueueArn --endpoint-url http://localhost:4576)
-aws lambda create-event-source-mapping \
-    --event-source-arn arn:aws:sqs:$REGION:queue:orderPlaced \
-    --function-name $LAMBDA_NAME \
-    --endpoint http://localhost:4574
+# Remove old event source mapping
+echo "Removing old SQS subscription..."
+EVENT_SOURCE_UUID=$(aws lambda list-event-source-mappings --query "EventSourceMappings[?EventSourceArn==\`arn:aws:sqs:elasticmq:000000000000:orderPlaced\`].UUID" --output text --region $REGION --endpoint http://localhost:4574)
+aws lambda delete-event-source-mapping --uuid $EVENT_SOURCE_UUID --region $REGION --endpoint http://localhost:4574
 
+# Bind function to `orderPlaced` SQS queue (assuming it was created)
+echo "Creating new SQS subscription for lambda..."
+aws lambda create-event-source-mapping \
+    --event-source-arn arn:aws:sqs:elasticmq:000000000000:orderPlaced \
+    --function-name $LAMBDA_NAME \
+    --batch-size 1 \
+    --endpoint http://localhost:4574
--- legacy-lambda/1-event-to-http/src/handler.py
+++ legacy-lambda/1-event-to-http/src/handler.py
@@ -1,17 +1,36 @@
-# import json
+import json
+import requests
+
+
+def failure(msg):
+    raise Exception(msg)
+
+
+def success(body):
+    return {"statusCode": 200, "body": json.dumps(body)}
 
 
 def handler(event, context):
     """
     Exercise 4
     Task: use the consumed SQS message in `event` to perform a POST call on the legacy Inventory service
+    We've added some code to help you get the message (there might be multiple) from the Lambda event.
     For this exercise to succeed do the following:
-    - Grab the messages from the `event` parameter using event["Records"]. Multiple messages might be available.
+    - Grab the records from the Lambda event (under the `Records` key in the `event` dict)
     - For each message:
-        - Grab the message body (using message["body"]) and parse the JSON
+        - Grab the message body (using message["body"]) and parse it into a python dict using `json.loads(body)`
         - Use the `requests` library to perform a HTTP POST to http://localhost:9010/inventory-api/v1/shipments
-        - Rely on the retry mechanism of SQS to make sure the POST is repeated until you get a 200 OK response code
+    - When all messages are processed, return a success message (using the success function, body can be anything)
     """
-    records = event["Records"]
-    print(records)
+    # Grab records
+    records = []
 
+    # For each SQS message
+    for message in records:
+        # Grab body
+
+        # Perform POST request
+        # r = requests.post(url, json=body)
+        pass
+
+    # Notify success
